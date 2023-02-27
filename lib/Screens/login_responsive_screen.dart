9c237cb1366d20a67f13c5b0599312681963e42e
klyct <takanashirikka.42@gmail.com>
2023-02-27 08:43:30 -0600

    3

--- lib/Screens/login_responsive_screen.dart
+++ lib/Screens/login_responsive_screen.dart
@@ -1,29 +1,55 @@
 import 'package:aa/Screens/login_screen.dart';
+import 'package:aa/components/background.dart';
 import 'package:aa/responsive.dart';
 import 'package:flutter/material.dart';
+
+import '../login_components/buttons.dart';
+import '../login_components/icon_image.dart';
+import '../login_components/users_info.dart';
 
 class LoginResponsiveScreen extends StatelessWidget {
   const LoginResponsiveScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Background(child: 
       Responsive(
           mobile: const LoginScreen(), 
           desktop: Row(
             children: [
-              Expanded(child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const[
-                  SizedBox(
-                    width: 300,
-                    child: LoginScreen(),
-                  )
-                ],
-              ))  
+              Expanded (
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: IconImage()
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            SizedBox(
+                              width: 450,
+                              child: UsersInfo(),
+                            )
+                          ],
+                      )
+                    ),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          SizedBox(
+                            width: 450,
+                            child: ButtonsLogin(),
+                          )
+                        ],
+                    )
+                    )
+                  ],
+                )
+            )  
           ],
         )
-    
-    );
+    ));
   }
 }

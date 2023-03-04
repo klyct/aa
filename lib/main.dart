f1e3dbf6d4fe93aad6f84cff1fa6ac8f505db024
klyct <takanashirikka.42@gmail.com>
2023-03-04 16:21:06 -0600

    onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:aa/provider/theme_provider.dart';
 import 'package:aa/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-
-import 'Screens/login_responsive_screen.dart';
+import 'Screens/onboarding_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+    const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {  
-
     return  ChangeNotifierProvider(
       create: (context)=>ThemeProvider(context),
       child: aaApp()
@@ -28,11 +29,10 @@ class  aaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     ThemeProvider theme = Provider.of<ThemeProvider>(context);
-
     return MaterialApp (
         theme: theme.getthemeData(),
         routes: getApplicationRoutes(),
-        home:  const LoginResponsiveScreen(),
+        home: OnboardingPage(),
       );
   }
 }

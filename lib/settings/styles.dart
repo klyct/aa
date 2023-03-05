8be0fd9b17a62b5bd9ee0af76c17438bb66c6d17
klyct <takanashirikka.42@gmail.com>
2023-03-05 00:49:10 -0600

    theme

--- lib/settings/styles.dart
+++ lib/settings/styles.dart
@@ -16,7 +16,27 @@ class StylesApp{
     return theme.copyWith(
       colorScheme: Theme.of(context).colorScheme.copyWith( 
         primary: const Color.fromARGB(255, 255, 14, 155))
-      
+    );
+  }
+
+  static ThemeData moonTheme(BuildContext context){
+    final ThemeData theme = ThemeData.from(
+      colorScheme: const ColorScheme(
+        brightness: Brightness.light, 
+        primary: Color.fromARGB(255, 179, 0, 54), 
+        onPrimary: Color.fromARGB(255, 179, 0, 54), 
+        secondary: Color.fromARGB(255, 110, 214, 255), 
+        onSecondary: Color.fromARGB(255, 110, 214, 255),
+        error: Color.fromARGB(255, 255, 0, 21), 
+        onError: Color.fromARGB(255, 255, 0, 21), 
+        background: Color.fromARGB(137, 179, 0, 54),  
+        onBackground: Color.fromARGB(137, 179, 0, 54),  
+        surface: Color.fromARGB(255, 2, 28, 49), 
+        onSurface: Color.fromARGB(255, 2, 28, 49),
+        ));
+    return theme.copyWith(
+      colorScheme: Theme.of(context).colorScheme.copyWith( 
+        primary: const Color.fromARGB(255, 179, 0, 54), )
     );
   }
 }
